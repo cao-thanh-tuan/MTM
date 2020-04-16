892bb2561ba73e4432aeadd4a64704a84c41f0bd
cao-thanh-tuan <tuanct@gmail.com>
2020-04-16 16:36:10 +0300

    Update UI

--- Pages/Classes/Index.cshtml.cs
+++ Pages/Classes/Index.cshtml.cs
@@ -19,11 +19,59 @@ namespace MTM.Pages.Classes
             _context = context;
         }
 
-        public IList<Class> Classes { get;set; }
+        public string NameSort { get; set; }
+        public string CitySort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public PaginatedList<Class> Classes { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
         {
-            Classes = await _context.Classes.ToListAsync();
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            CitySort = sortOrder == "city" ? "city_desc" : "city";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+
+
+            IQueryable<Class> classIQ = from d in _context.Classes select d;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                classIQ = classIQ.Where(c => c.Name.Contains(searchString)
+                                       || c.City.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    classIQ = classIQ.OrderByDescending(c => c.Name);
+                    break;
+                case "city":
+                    classIQ = classIQ.OrderBy(c => c.City);
+                    break;
+                case "city_desc":
+                    classIQ = classIQ.OrderByDescending(c => c.City);
+                    break;
+                default:
+                    classIQ = classIQ.OrderBy(c => c.Name);
+                    break;
+            }
+
+            int pageSize = 10;
+            Classes = await PaginatedList<Class>.CreateAsync(classIQ, pageIndex ?? 1, pageSize);
         }
     }
 }
